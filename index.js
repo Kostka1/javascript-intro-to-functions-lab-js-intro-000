6e564b3e581da7a119f39248a68adced144647b2
Stanislaus Kostka <stanislauskostka1@gmail.com>
2017-08-22 12:11:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ function whisper(string) {return string.toLowerCase()}
 function logShout(string) {console.log(string.toUpperCase())}
 function logWhisper(string) {console.log(string.toLowerCase())}
 function sayHiToGrandma(string) {if (string.toLowerCase())
-{return 'I can\'t hear you!';}
-( string.toUpperCase()) else if {
-    return "YES INDEED!";
-} if (string === 'I love you, Grandma.') {
-  return 'I love you, too.';
-}
-}
+{return 'I can\'t hear you!';}}
+function sayHiToGrandma(string) {else if (string.toLowerCase())
+{return 'I can\'t hear you!';}}
+
